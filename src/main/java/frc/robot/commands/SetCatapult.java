c342306e020cddd953b72ff71e23091beb2719c8
adua448 <adua448@student.fuhsd.org>
2025-09-10 02:44:51 +0000

    first try

---
+++ src/main/java/frc/robot/commands/SetCatapult.java
@@ -0,0 +1,39 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.subsystems.Catapult;
+import frc.robot.subsystems.Catapult.CatapultMode;
+
+public class SetCatapult extends InstantCommand {
+  private Catapult catapult;
+
+
+  public SetCatapult() {
+    this.catapult = Catapult.getInstance();
+
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    catapult.setCatapultMode(CatapultMode.ON);
+  }
+}
+//   // Called every time the scheduler runs while the command is scheduled.
+//   @Override
+//   public void execute() {
+    
+//   }
+
+//   // Called once the command ends or is interrupted.
+//   @Override
+//   public void end(boolean interrupted) {
+    
+//   }
+
+//   // Returns true when the command should end.
+//   @Override
+//   public boolean isFinished() {
+//     return false;
+//   }
+// }
