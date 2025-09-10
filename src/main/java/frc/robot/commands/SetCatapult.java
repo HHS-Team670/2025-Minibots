c08307e64e4d64e4ea60ec023c49a94e0ef60a73
Eric Chen <94388267+EricChen12221@users.noreply.github.com>
2025-09-09 20:30:34 -0700

    works

--- src/main/java/frc/robot/commands/SetCatapult.java
+++ src/main/java/frc/robot/commands/SetCatapult.java
@@ -1,22 +1,26 @@
 package frc.robot.commands;
 
-import frc.robot.joystickUtils.XboxJoysticButtons;
 import frc.robot.subsystems.Catapult;
 import frc.robot.subsystems.Catapult.CatapultMode;
+
+
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 
 public class SetCatapult extends InstantCommand {
 
     private Catapult mCatapult;
+    private CatapultMode mMode;
     
-    public SetCatapult()
+    public SetCatapult(CatapultMode mode)
     {
         this.mCatapult= Catapult.getInstance();
+        mMode= mode;
+
     }
       
  @Override
   public void initialize() {
-    mCatapult.setCatapultMode(CatapultMode.ON);
+    mCatapult.setCatapultMode(mMode);
     }
 
   
