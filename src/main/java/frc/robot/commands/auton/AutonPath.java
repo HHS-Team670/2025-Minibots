d20935eb207fc63afbff00b2cc90918997a6c927
Atripta Kafle <akafle661@student.fuhsd.org>
2025-09-11 00:22:08 +0000

    git commi

--- src/main/java/frc/robot/commands/auton/AutonPath.java
+++ src/main/java/frc/robot/commands/auton/AutonPath.java
@@ -1,14 +1,17 @@
 package frc.robot.commands.auton;
 
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.commands.drivetrain.DriveForward;
 
-public class AutonPath extends SequentialCommandGroup {
+
+public class AutonPath extends SequentialCommandGroup { 
     // Sequentially runs commands in an auton path
     // NOTE: You can also create parallel command groups which run commands
     // simultaneously (these are also treated like commands and can be nested within
     // sequential command groups)
     public AutonPath() {
         addCommands(
+            new DriveForward(1,10)
         );
     }
 }
