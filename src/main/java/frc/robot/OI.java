3ecbbb85feaf1ba79b387a7f73f97c95c47ae5c1
cwei946 <cwei946@student.fuhsd.org>
2025-09-17 02:43:15 +0000

    setCatapult.java

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -2,9 +2,11 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.commands.SetCatpult;
 import frc.robot.commands.drivetrain.ArcadeDrive;
 import frc.robot.joystickUtils.XboxJoysticButtons;
 import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Catapult.CatapultMode;
 
 public final class OI {
   // Controllers
@@ -21,6 +23,10 @@ public final class OI {
   }
 
   public static void configureDriverControls() {
+    XboxJoysticButtons.Driver_ButtonX.onTrue(new SetCatpult(CatapultMode.OFF));
+    // XboxJoysticButtons.Driver_ButtonX.onTrue(new SetCatapult(CatapultMode.ON));
+    // XboxJoysticButtons.Driver_ButtonX.onTrue(new SetCatapult(CatapultMode.ON));
+    // XboxJoysticButtons.Driver_ButtonY.onTrue(new SetCatapult(CatapultMode.OFF));
   }
 
   // Returns the command that will be set as the drive command during tele-op
--- src/main/java/frc/robot/commands/SetCatpult.java
+++ src/main/java/frc/robot/commands/SetCatpult.java
@@ -1,31 +1,22 @@
 package frc.robot.commands;
 
-  import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.subsystems.Catapult;
+import frc.robot.subsystems.Catapult.CatapultMode;
 
-  public class SetCatpult extends Command {
 
-
+public class SetCatpult extends InstantCommand {
+  private Catapult catapult;
+  CatapultMode mode;
+  public SetCatpult(CatapultMode mode){
+    this.catapult = Catapult.getInstance();
+    this.mode = mode;
+  }
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    catapult.setCatapultMode(this.mode);
     
   }
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    
-  }
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-    
-  }
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return true;
-  }
-}
+} 
