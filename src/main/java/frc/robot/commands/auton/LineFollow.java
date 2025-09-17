387c056132413a3cc9daccc5df0e01307de47661
aivanov0255 <aivanov255@student.fuhsd.org>
2025-09-16 19:35:35 -0700

    Finished Line Follow

--- src/main/java/frc/robot/commands/auton/LineFollow.java
+++ src/main/java/frc/robot/commands/auton/LineFollow.java
@@ -10,25 +10,28 @@ public class LineFollow extends Command {
     private double speed;
     private Drivetrain drivetrain;
     private double turnSpeed;
+    private static float bottomInterval = 0.0f;
+    private static float topInterval = 0.85f;
 
     public LineFollow(double speed, double turnSpeed) {
         this.sensor = ReflectiveSensor.getInstance();
         this.speed = speed;
         this.drivetrain = Drivetrain.getInstance();
         this.turnSpeed = turnSpeed;
+        addRequirements(drivetrain, sensor);
     }
 
     @Override
     public void initialize() {
-        
+        drivetrain.arcadeDrive(0, 0);
     }
 
     public boolean isLeft() {
-        return this.sensor.leftValue() >= 0.75 && this.sensor.rightValue() <= 0.9;
+        return this.sensor.leftValue() >= bottomInterval && this.sensor.leftValue() <= topInterval;
     }
 
     public boolean isRight() {
-        return this.sensor.rightValue() >= 0.75 && this.sensor.rightValue() <= 0.9;
+        return this.sensor.rightValue() >= bottomInterval && this.sensor.rightValue() <= topInterval;
     }
 
     @Override
@@ -36,11 +39,16 @@ public class LineFollow extends Command {
         System.out.println(this.sensor.rightValue() + ", " + this.sensor.leftValue());
 
         if (isLeft() && isRight()) {
-            drivetrain.arcadeDrive(speed, 0);
+            this.drivetrain.arcadeDrive(speed, 0);
+            // System.out.println("Online");
         } else if (!isLeft() && isRight()) {
-            drivetrain.arcadeDrive(0, turnSpeed);
+            this.drivetrain.arcadeDrive(0, turnSpeed);
+            // System.out.println("swerve left");
+
         } else if (isLeft() && !isRight()) {
-            drivetrain.arcadeDrive(0, turnSpeed);
+            this.drivetrain.arcadeDrive(0, turnSpeed);
+            // System.out.println("swerve right");
+
         }
     }
 
@@ -51,6 +59,7 @@ public class LineFollow extends Command {
 
     @Override
     public void end(boolean interrupted) {
-        System.out.println("Finished with line");
+        System.out.println("Finished following line");
+        drivetrain.arcadeDrive(0, 0);
     }
 }
