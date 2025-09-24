d92961e33ae381b87564700ca98c313aceec8ad7
51n <WesleyHsu10@gmail.com>
2025-09-24 16:36:18 -0700

    idk

--- src/main/java/frc/robot/commands/Turn.java
+++ src/main/java/frc/robot/commands/Turn.java
@@ -1,17 +1,38 @@
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.commands.drivetrain.DriveForward;
-import frc.robot.commands.drivetrain.TurnDegrees;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.ReflectiveSensor;
+import edu.wpi.first.wpilibj2.command.Command;
 
-public class Turn extends SequentialCommandGroup {
-    public Turn() {
-        addCommands (
-            new DriveForward(1, 12),
-            new TurnDegrees(1, 90),
-            new DriveForward(1, 8),
-            new TurnDegrees(1, -90),
-            new DriveForward(1, 12)
-        );
+public class Turn extends Command {
+    ReflectiveSensor rf;
+    Drivetrain mDrivetrain;
+    double lValue, rValue, tapeColor = 0.8;
+
+    public Turn () {
+        this.rf = ReflectiveSensor.getInstance();
+        this.mDrivetrain = Drivetrain.getInstance();
+        addRequirements(mDrivetrain);
+    }
+
+    @Override
+    public void initialize () {
+    }
+
+    @Override
+    public void execute () {
+        lValue = rf.leftValue();
+        rValue = rf.rightValue();
+
+        mDrivetrain.arcadeDrive(1,0.8);
+    }
+
+    @Override
+    public boolean isFinished() {
+        return (lValue >= tapeColor && rValue >= tapeColor);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
     }
 }
--- src/main/java/frc/robot/commands/auton/AutonPath.java
+++ src/main/java/frc/robot/commands/auton/AutonPath.java
@@ -11,23 +11,19 @@ public class AutonPath extends SequentialCommandGroup {
     // sequential command groups)
     public AutonPath() {
         addCommands (
-            // new DriveForward(1, 5),
-            // new TurnDegrees(1, 90),
-
-            // new DriveForward(1, 5),
-            // new TurnDegrees(1, 90),
-
-            // new DriveForward(1, 5),
-            // new TurnDegrees(1, 90),
-
-            // new DriveForward(1, 5),
-            // new TurnDegrees(1, 90)
-
-            new DriveForward(1, 12),
+            new DriveForward(1, 5),
             new TurnDegrees(1, 90),
-            new DriveForward(1, 8),
-            new TurnDegrees(1, -90),
-            new DriveForward(1, 12)
+
+            new DriveForward(1, 5),
+            new TurnDegrees(1, 90),
+
+            new DriveForward(1, 5),
+            new TurnDegrees(1, 90),
+
+            new DriveForward(1, 5),
+            new TurnDegrees(1, 90)
+
+            
         );
     }
 }
