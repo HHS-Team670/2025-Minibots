1a2c12f8a98893cdbb772fb0e59f42955be9737d
atrimbakkar126 <atrimbakkar126@student.fuhsd.org>
2025-09-24 23:51:51 +0000

    basic learning of code

---
+++ src/main/java/frc/robot/commands/SetCatapault.java
@@ -0,0 +1,33 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Catapult.CatapultMode;
+
+public class SetCatapault extends Command{
+    public SetCatapault(CatapultMode catapultMode) {
+        
+    }
+
+    @Override
+    public void initialize() {
+
+    }
+
+    @Override
+    public void execute() {
+
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+    @Override
+    public void end(boolean interrupted) {
+
+    }
+
+    
+}
+
+
