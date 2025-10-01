7f16c21a87aeee243583313e8175e7e12cdb6900
Amandehar Prasetty <81485915+aman60dehar@users.noreply.github.com>
2025-10-01 02:41:28 +0000

    setcatapult

---
+++ src/main/java/frc/robot/commands/SetCatapult.java
@@ -0,0 +1,20 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.subsystems.Catapult;
+import frc.robot.subsystems.Catapult.CatapultMode;
+
+public class SetCatapult extends InstantCommand {
+    Catapult catapult;
+    CatapultMode mode;
+
+    public SetCatapult(CatapultMode mode) {
+        this.catapult = Catapult.getInstance();
+        this.mode = mode;
+    }
+
+    @Override
+    public void initialize() {
+        catapult.setCatapultMode(mode);
+    }
+}
