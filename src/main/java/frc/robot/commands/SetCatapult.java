685a23683c1be91ad7d6a28b2bc821f7c33345ce
ClawlessTiger <aarush.joglekar@gmail.com>
2025-10-01 08:44:52 -0700

    base

---
+++ src/main/java/frc/robot/commands/SetCatapult.java
@@ -0,0 +1,21 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.subsystems.Catapult;
+import frc.robot.subsystems.Catapult.CatapultMode;
+
+public class SetCatapult extends InstantCommand {
+    private Catapult catapult;
+    private CatapultMode mode;
+    
+    public SetCatapult(CatapultMode mode) {
+        this.catapult = Catapult.getInstance();
+        this.mode = mode;
+        addRequirements(catapult);
+    }
+
+    @Override
+    public void initialize() {
+        catapult.setCatapultMode(mode);
+    }
+}
